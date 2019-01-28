af5948369cfcd9623482d03800c36d6bb90d8cb8
James Bell <Noxachron@gmail.com>
2019-01-28 11:34:16 -0500

    added mlab database

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://bryantheuser:abc12345@ds255253.mlab.com:55253/bryan-james-dsa-learn-python',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
