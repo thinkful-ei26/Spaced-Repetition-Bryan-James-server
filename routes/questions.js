f959be4cbb560e585fdac03f14e17c38ac4f6ca7
James Bell <Noxachron@gmail.com>
2019-01-29 20:43:58 -0500

    changed questions to find by id instead of question text

--- routes/questions.js
+++ routes/questions.js
@@ -10,36 +10,36 @@ questionRouter.use('/', passport.authenticate('jwt', {session : false, failWithE
 questionRouter.post('/data', (req, res, next)=>{
   // this route handles algorithm logic
   //const userId = req.user.id;
-  const question = req.body.Question;
+  const id = req.body.id;
   const answer = req.body.Answer;
   //validate question and answer are legit
   //check if answer is right or wrong
-  const whichQuestion = User.find({questionPool: question});
-  if(answer===whichQuestion.answer){
   
-      let rightReply = {"feedback": "Some correct message here",
-    
-      };
-    res.json(rightReply);
+  return Question.find({"_id": id})
+  .then(([whichQuestion, ...data])=>{
+    if(answer===whichQuestion.Answer){
+  
+      let rightReply = {"feedback": "Correct"};
+    return res.json(rightReply);
   
   }
-
-  let wrongReply = {"feedback": `Sorry the correct answer is: ${whichQuestion.answer}`,
-  
+  let wrongReply = {"feedback": `Sorry the correct answer is: ${whichQuestion.Answer}`,
   };
-  res.json(wrongReply);
+  return res.json(wrongReply);
+  });
+  
 });
 // Route to handle client asking for a question:
 questionRouter.post('/next', (req, res, next)=>{
   //this route handles the sending of next question to user
   //const userId = req.user.id;
-  const question = req.body.Question;
+  const id = req.body.id;
   // this route is called for both the initial question and skip next question button
-  if(!question){
+  if(!id){
     //here the question request is null, so we just send back the first question on the list
     return Question.find()//returns 10 questions from mLab
     .then((data)=>{
-      res.json(data[0]);
+      return res.json(data[0]);
     })
     .catch(()=>{});
   }
@@ -49,20 +49,36 @@ questionRouter.post('/next', (req, res, next)=>{
   return Question.find()
   .then((results)=>{
  //logic here to loop through all questions in db and grab the next one.
- //console.log("the next question, ", results);
- //let nextQuestion = null;
- for(let i = 0; i<results.length -1; i++){
+  //let nextQuestion = null;
+ for(let i = 0; i<results.length; i++){
    //loop through all questions in db
-   if(results[i]["Question"] === question){
+   if(results[i]["_id"] == id){
      //found the question client has sent us
-     let nextQuestion = results[i+1];
-     //the next one on the list is sent back to client
-     res.json(nextQuestion);
+     let nextQuestion;
+     if(i=== results.length-1){
+       //the last question in the pool, send them the first till we have algorithm
+      nextQuestion = results[0];
+     } else {
+       //the next one on the list is sent back to client
+      nextQuestion = results[i+1];
+     }
+     return res.json(nextQuestion);
+    
    }
  }
- //console.log("Uh-hoh");
+
+ return res.sendStatus(400);
   })
-  .catch(()=>{});
- 
+  .catch(()=>{
+    return res.sendStatus(400);
+  });
+  
+});
+questionRouter.post('/all', (req,res, next)=>{
+  //const userId = req.user.id;
+  // return User.find({id: userId})// get current users' questions from questionPool
+  return Question.find()
+  .then(results=> res.json(results))
+  .catch(err=>next(err));
 });
 module.exports =  questionRouter;
